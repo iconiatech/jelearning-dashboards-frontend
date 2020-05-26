ec00f4087be72c617ac0bdf06dfac954edae8ac3
Gichia <petergichia35@gmail.com>
2020-05-26 15:39:00 +0300

    [Feature #172972476] added the footer to the app layout.

---
+++ src/components/layout/footer/footer.component.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="sticky-footer bg-white">
+            <div className="container my-auto">
+                <div className="copyright text-center my-auto">
+                    <span>Copyright &copy; Iconia 2020</span>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
