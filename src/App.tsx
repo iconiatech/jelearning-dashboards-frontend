ff18e6314889f308107c8bbe9384ef6a70bf4391
Gichia <petergichia35@gmail.com>
2020-05-26 08:10:05 +0300

    [Starts #172972476] updated the base file and added the admin summary component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import AdminSummary from "./components/admin-summary/admin-summary.component";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <AdminSummary />
     </div>
   );
 }
