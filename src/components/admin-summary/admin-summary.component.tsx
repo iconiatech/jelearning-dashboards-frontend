9c10530ab1631abdcd31f58017dacf40b3a93a1e
Gichia <petergichia35@gmail.com>
2020-05-26 08:11:26 +0300

    [Feature #172972476] created a new component for the admin summary dashboard.

---
+++ src/components/admin-summary/admin-summary.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AdminSummary = () => {
+    return <div>
+        <h1>Admin Dashboard</h1>
+
+        <div>
+            table here
+        </div>
+    </div>
+}
+
+export default AdminSummary;
