4d0000c24ee49ca369ab23ab1f4d03bc1f425510
Gichia <petergichia35@gmail.com>
2020-05-27 13:14:21 +0300

    [Feature #17297276] created a component for the scroll to top button.

---
+++ src/components/layout/scroll-to-top/scroll-to-top.component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ScrollToTop: React.FC = () => {
+    return (
+        <a className="scroll-to-top rounded" href="#page-top">
+            <i className="fas fa-angle-up" />
+        </a>
+    )
+}
+
+export default ScrollToTop;
