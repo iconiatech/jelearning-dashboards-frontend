52c30d85728cbc40bf9b43f1e65aa192c9b271b2
Gichia <petergichia35@gmail.com>
2020-05-27 13:15:32 +0300

    [Feature #17297276] added proptypes to the kpi card component.

--- src/components/kpi-card/kpi-card.component.tsx
+++ src/components/kpi-card/kpi-card.component.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 interface KPICardProps {
     xlSize: number;
@@ -17,8 +18,8 @@ const KPICard: React.FC<KPICardProps> = ({ xlSize, mdSize, color, title, leadTit
             <div className={`card bg-gradient-${color} border-0"`}>
                 <div className="card-body">
                     <h5 className="text-white-50">{title}</h5>
-    <div className="mb-3"><span className="display-4 text-white">{leadValue}</span><span className="text-white-50"> {leadTitle}</span></div>
-    <div className="text-warning"><span className="h5">{subValue}</span><span> {subTitle}</span></div>
+                    <div className="mb-3"><span className="display-4 text-white">{leadValue}</span><span className="text-white-50"> {leadTitle}</span></div>
+                    <div className="text-warning"><span className="h5">{subValue}</span><span> {subTitle}</span></div>
                 </div>
             </div>
         </div>
@@ -27,6 +28,17 @@ const KPICard: React.FC<KPICardProps> = ({ xlSize, mdSize, color, title, leadTit
 
 KPICard.defaultProps = {
     color: "primary",
+}
+
+KPICard.propTypes = {
+    color: PropTypes.string,
+    xlSize: PropTypes.number.isRequired,
+    mdSize: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    leadTitle: PropTypes.string.isRequired,
+    subTitle: PropTypes.string.isRequired,
+    leadValue: PropTypes.string.isRequired,
+    subValue: PropTypes.string.isRequired,
 }
 
 export default KPICard;
