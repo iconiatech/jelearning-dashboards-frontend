888a3ed04faa975f8d88d50c3926298d6c5d3673
Gichia <petergichia35@gmail.com>
2020-05-29 19:30:41 +0300

    [Feature #172972130] added the app provider and store to the app. also moved the browser router from app component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+import store from "./redux/store";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
-serviceWorker.unregister();
