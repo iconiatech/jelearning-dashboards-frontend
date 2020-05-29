b5742a41876e50d50c400477150f355508202417
Gichia <petergichia35@gmail.com>
2020-05-29 19:32:56 +0300

    [Feature #172972130] added redux and react router to the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
 
+import { RootState } from "./redux/root-reducer";
+import { UserState } from "./redux/user/user.types";
+
+import Login from "./components/pages/login/login.component";
 import Topbar from "./components/layout/topbar/topbar.component";
 import Footer from "./components/layout/footer/footer.component";
-import Dashboard from "./components/dashboard/dashboard.component";
 import Sidebar from "./components/layout/sidebar/sidebar.component";
+import Dashboard from "./components/pages/dashboard/dashboard.component";
 import MainContent from "./components/layout/main-content/main-content.component";
 import ScrollToTop from "./components/layout/scroll-to-top/scroll-to-top.component";
 import ContentWrapper from "./components/layout/content-wrapper/content-wrapper.component";
@@ -16,24 +22,41 @@ import "./App.css";
 import "popper.js";
 import "bootstrap/dist/js/bootstrap";
 
-const App: React.FC = () => {
-  return (
-    <div id="wrapper">
-      	<Sidebar/>
-        <ContentWrapper>
-			<MainContent>
-				{/* Topbar */}
-				<Topbar />
-				{/* Content Container */}
-				<ContentContainer>
-					<Dashboard />
-				</ContentContainer>
-			</MainContent>
-			<Footer />
-        </ContentWrapper>
-		<ScrollToTop />
-    </div>
-  );
+interface AppProps {
+	user: UserState;
 }
 
-export default App;
+const App: React.FC<AppProps> = ({ user }) => {
+
+	if (!user.isAuthenticated) {
+		return (
+			<Login />
+		)
+	}
+
+	return (
+		<div id="wrapper">
+			<Sidebar/>
+			<ContentWrapper>
+				<MainContent>
+					{/* Topbar */}
+					<Topbar />
+					{/* Content Container */}
+					<ContentContainer>
+						<Switch>
+							<Route exact path="/" component={Dashboard} />
+						</Switch>
+					</ContentContainer>
+				</MainContent>
+				<Footer />
+			</ContentWrapper>
+			<ScrollToTop />
+		</div>
+	);
+}
+
+const mapStateToProps = (state: RootState) => ({
+	user: state.user,
+});
+
+export default connect(mapStateToProps)(App);
