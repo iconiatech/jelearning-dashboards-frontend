fda731e44e12a1ecf21c03abab4ef47e3916a81e
Gichia <petergichia35@gmail.com>
2020-05-30 22:54:46 +0300

    [Feature #172972130] deleted the redux action.

--- src/redux/user/user.actions.ts
+++
@@ -1,6 +0,0 @@
-import { UserState, SET_CURRENT_USER } from "./user.types";
-
-export const setCurrentUser = (user: UserState) => ({
-    type: SET_CURRENT_USER,
-    payload: user.isAuthenticated
-})
