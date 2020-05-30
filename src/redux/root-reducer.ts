bdce106c80fd647a7d5832fae3f9c8ad99e2e3c1
Gichia <petergichia35@gmail.com>
2020-05-30 20:03:09 +0300

    [Feature #172972130] updated the user reducer to the new reducer.

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import userReducer from "./user/user.reducer";
+import { userReducer } from "./modules/user/user";
 
 const rootReducer = combineReducers({
     user: userReducer
