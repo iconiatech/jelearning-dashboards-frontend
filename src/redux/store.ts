9fed71ca0baac57ecc60a7c2b6d13737abb2749e
Gichia <petergichia35@gmail.com>
2020-06-02 20:52:42 +0300

    [Feature #172972130] updated the store to include redux persist.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 // const middlewares = [];
 
-export default createStore(rootReducer, applyMiddleware());
+export const store = createStore(rootReducer, applyMiddleware());
+
+export const persistor = persistStore(store);
+
+export default { store, persistor };
