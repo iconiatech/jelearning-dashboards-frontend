a879b82990ffeadad7aacb7c0bce7b7f8d33a8e1
Gichia <petergichia35@gmail.com>
2020-06-03 09:35:11 +0300

    [Feature #172972130] moved the sidebar links to its own file

--- src/components/layout/sidebar/sidebar.component.tsx
+++ src/components/layout/sidebar/sidebar.component.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import SidebarLinks from "../sidebar-links/sidebar-links.component";
+
+
 const Sidebar: React.FC = () => {
     return (
         <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
@@ -32,20 +35,7 @@ const Sidebar: React.FC = () => {
             </div>
 
             {/* Nav Item - Admin Collapse Menu */}
-            <li className="nav-item">
-                <a className="nav-link collapsed" href="#!" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    <i className="fas fa-user-cog" />
-                <span>Admin</span>
-                </a>
-                <div id="collapseOne" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                <div className="bg-white py-2 collapse-inner rounded">
-                    <h6 className="collapse-header">Admin Dashboards:</h6>
-                    <a className="collapse-item" href="#!">Teachers</a>
-                    <a className="collapse-item" href="#!">Students</a>
-                    <a className="collapse-item" href="#!">Grades</a>
-                </div>
-                </div>
-            </li>
+            { <SidebarLinks /> }
   
             {/* Sidebar Toggler (Sidebar) */}
             <div className="text-center d-none d-md-inline">
